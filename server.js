485ad7e7eb711ac411cbd96b272c5fe14a678364
AlexRutledge1 <31017536+AlexRutledge1@users.noreply.github.com>
2021-11-28 08:43:05 -0500

    Update server.js

--- server.js
+++ server.js
@@ -16,10 +16,12 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.static(path.join(__dirname, './frontend/build')))
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './frontend/build'))
-})
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("/*", function(req, res) {
+      res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    });
+}
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
