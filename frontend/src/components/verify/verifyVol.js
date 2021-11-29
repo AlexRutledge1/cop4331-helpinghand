d5b7d09fbbf16e29a19f251a3b587b53d672d991
AlexRutledge1 <31017536+AlexRutledge1@users.noreply.github.com>
2021-11-29 10:13:54 -0500

    Verify Email UI update

--- frontend/src/components/verify/verifyVol.js
+++ frontend/src/components/verify/verifyVol.js
@@ -1,72 +1,115 @@
-import React, { useState } from "react"
+import React, {useState} from "react"
 import { makeStyles } from '@material-ui/core/styles';
-import TaskCard from '../volunteertaskcard/taskcard.js'
 import Grid from '@material-ui/core/Grid';
 import Appbar from "../appbar/appbar.js";
+import { useHistory, useParams } from "react-router-dom";
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import { useLocation, useParams } from "react-router-dom";
-import axios from 'axios';
 import { connect } from "react-redux";
 import { setAreas } from '../../redux/actions.js';
-import { useHistory } from "react-router-dom";
 
 const buildPath = require('../../redux/buildPath');
 
 const useStyles = makeStyles((theme) => ({
     root: {
-      marginTop: 40,
+        ...theme.typography.button,
+        backgroundColor: theme.palette.background.paper,
+    },
+    string: {
+        marginTop: '20px',
+        marginBottom: '20px',
+        fontSize: '20px',
+    },
+    welcome: {
+        margin: '15px',
+        marginBottom: '10px',
+        fontSize: '28px',
+        fontWeight: 'bold',
+    },
+    starter: {
+        fontSize: '18px',
+        marginBottom: '60px',
+
+    },
+    padding: {
+        marginTop: '20px',
+        marginBottom: '20px',
     },
     button: {
-      marginTop: '40px',
-      width: '110px',
-      height: '40px',
-      backgroundColor: "#27AE60",
-      color: '#FFFFFF'
+        width: '200px',
+        height: '60px',
+        fontSize: '22px',
+        background: '#27AE60',
+        boxShadow: '0px 4px 4px rgba(154, 154, 154, 0.25)',
+        radius: '10px',
+        color: '#FFFFFF'
     },
+    smallbutton: { 
+        marginTop:'20px',
+        width: '120px',
+        height: '40px',
+        fontSize: '13px',
+        background: '#27AE60',
+        boxShadow: '0px 4px 4px rgba(154, 154, 154, 0.25)',
+        radius: '10px',
+        color: '#FFFFFF'
+    },
+    image: {
+        marginTop: '15px',
+        height: '200px',
+    },
+    link: {
+        alignItems: "center",
+        color: "#27AE60",
+    }
+
 }));
 
-function VerifyVolPage(props)
+function VerifyVol()
 {
-  let {token} = useParams();
-  const history = useHistory();
-  async function handleSubmit(){
-    try{
-      const res = await fetch(buildPath('/vol/verify/') + token, {method: 'GET', headers:{'Content-Type':'application/json'}});
-      let response = JSON.parse(await res.text());
-      if (response.success)
-      {
-        history.push("/");
-      }
-      else
-      {
-        alert(response.error);
-      }
+    let {token} = useParams();
+    const history = useHistory();
+    async function handleSubmit(){
+        try{
+        const res = await fetch(buildPath('/vol/verify/') + token, {method: 'GET', headers:{'Content-Type':'application/json'}});
+        let response = JSON.parse(await res.text());
+        if (response.success)
+        {
+            history.push("/");
+        }
+        else
+        {
+            alert(response.error);
+        }
+        }
+        catch(e)
+        {
+        alert(e);
+        }
     }
-    catch(e)
-    {
-      alert(e);
-    }
-  }
     const classes = useStyles();
-
     return (
-        <>
-            <Grid container
+        <div>
+            <Appbar title="Verify Email" type="Volunteer"/>
+            <Grid
+                container
                 direction="column"
-                justify="center"
+                justify="space-between"
                 alignItems="center"
-                className={classes.root}
-                spacing={4}
-          ></Grid>
+            >
+            {/* <Grid item>
+            <img className={classes.image} src="/images/volunteer.png"></img>
+            </Grid> */}
+            
             <Grid item>
-                <Button 
+            <Button 
                 className={classes.smallbutton}
-                onClick={(handleSubmit)}>Verify Email
+                onClick={(handleSubmit)}>Verify
             </Button>
             </Grid>
-        </>
+            </Grid>
+        </div>
     )
 }
 
@@ -76,4 +119,4 @@ const mapDispatchToProps = { areaAction: setAreas }
 export default connect(
   null,
   mapDispatchToProps
-)(VerifyVolPage)
+)(VerifyVol)
