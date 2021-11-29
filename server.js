e74044c703aba31e577bfc0a461fcf0c83cee4bd
AlexRutledge1 <31017536+AlexRutledge1@users.noreply.github.com>
2021-11-28 23:45:50 -0500

    Update server.js

--- server.js
+++ server.js
@@ -16,10 +16,17 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.static(path.join(__dirname, './frontend/build')))
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './frontend/build'))
-})
+if (process.env.NODE_ENV === 'production') 
+{
+  // Set static folder
+  app.use(express.static('frontend/build'));
+
+  app.get('*', (req, res) => 
+ {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
