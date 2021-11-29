dc0e468d7f9dd7f25cb58b59c3d0cda59efb58ed
AlexRutledge1 <31017536+AlexRutledge1@users.noreply.github.com>
2021-11-29 00:15:34 -0500

    Update verifyVol.js

--- frontend/src/components/verify/verifyVol.js
+++ frontend/src/components/verify/verifyVol.js
@@ -33,8 +33,7 @@ function VerifyVolPage(props)
   const history = useHistory();
   async function handleSubmit(){
     try{
-      const res = await fetch(buildPath('/vol/verify/') + token, {method: 'GET',
-        body: js, headers:{'Content-Type':'application/json'}});
+      const res = await fetch(buildPath('/vol/verify/') + token, {method: 'GET', headers:{'Content-Type':'application/json'}});
       let response = JSON.parse(await res.text());
       if (response.success)
       {
